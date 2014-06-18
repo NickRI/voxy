e61cc160078246219756b91afd188347c64bea53
Nick Roshin <denebb808@gmail.com>
2014-06-18 13:49:27 +0400

    Add chapters support

--- app/javascripts/filters.js
+++ app/javascripts/filters.js
@@ -1,14 +1,34 @@
 var voxyFilters = angular.module('voxyFilters', []);
 
-voxyFilters.filter('chapters', ['$rootScope', function($rootScope) {
-	return function(sentenses, showByChapters) {
-		if (showByChapters && $rootScope.chapters && $rootScope.chapters.length > 0) {
-			var chapters = $rootScope.chapters.filter(function(chapter) {
-				return chapter.id <= $rootScope.selectedIndex || chapter.id > $rootScope.selectedIndex;
-			}).slice(1, 3);
-			return sentenses.filter(function(sentense) {
-				return sentense.id >= chapters[0].id && sentense.id < chapters[1].id;
-			});
+voxyFilters.filter('chapters', [function() {
+	return function(sentenses, showByChapters, selectedIndex, chapters) {
+		if (showByChapters) {
+			if (chapters && chapters.length > 0) {
+				var chapersIds = chapters.map(function(chapter) { return chapter.id; });
+				var minChapter = Math.min.apply(null, chapersIds);
+				var maxChapter = Math.max.apply(null, chapersIds);
+
+				if (minChapter > selectedIndex) {
+					return sentenses.filter(function(sentense) {
+						return sentense.id >= 0 && sentense.id < minChapter;
+					});
+				} else if (maxChapter <= selectedIndex) {
+					return sentenses.filter(function(sentense) {
+						return sentense.id >= maxChapter && sentense.id < sentenses.length;
+					});
+				} else {
+
+					var nextChapter = chapersIds.filter(function(chapter) {
+						return selectedIndex < chapter;
+					}).slice(0, 1).shift();
+
+					var currentChapter = chapersIds[chapersIds.indexOf(nextChapter) - 1];
+
+					return sentenses.filter(function(sentense) {
+						return sentense.id >= currentChapter && sentense.id < nextChapter;
+					});
+				}
+			}
 		} else {
 			return sentenses;
 		}
