e69b81ef2cffb0aef61e96e9888aa64898e8b50e
Nick Roshin <denebb808@gmail.com>
2014-06-26 06:54:47 +0400

    Preload angular-bootstrap templates

--- app/javascripts/controllers.js
+++ app/javascripts/controllers.js
@@ -1,1 +1,1 @@
-var voxyControllers = angular.module('voxyControllers', ['ui.bootstrap', 'ngStorage', 'ngAnimate', 'voxyDirectives', 'voxyServices', 'voxyFilters']);
+var voxyControllers = angular.module('voxyControllers', ['ui.bootstrap', 'ui.bootstrap.tpls', 'ngStorage', 'ngAnimate', 'voxyDirectives', 'voxyServices', 'voxyFilters']);
